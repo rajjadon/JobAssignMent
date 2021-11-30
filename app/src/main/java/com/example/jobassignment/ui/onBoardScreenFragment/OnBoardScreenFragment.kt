20bcfd504fe8ed256945221b8bcbc2d324958402
Raj Pratap Singh Jadon <raj.pratap@applaunch.io>
2021-11-30 21:07:25 +0530

    put flow collect on start of fragment

--- app/src/main/java/com/example/jobassignment/ui/onBoardScreenFragment/OnBoardScreenFragment.kt
+++ app/src/main/java/com/example/jobassignment/ui/onBoardScreenFragment/OnBoardScreenFragment.kt
@@ -1,6 +1,5 @@
 package com.example.jobassignment.ui.onBoardScreenFragment
 
-import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -21,7 +20,14 @@ class OnBoardScreenFragment : BaseFragment<FragmentOnBoardScreenBinding>() {
     override fun getFragmentLayout() = R.layout.fragment_on_board_screen
 
     override fun implementApiCallsDataReceiver() {
-        /*Not yet implemented*/
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                sessionManager.isLoggedIn.collect {
+                    if (it)
+                        findNavController().navigate(FragmentNavigator.OPEN_HOME_SCREEN_FROM_ON_BOARD_SCREEN.navigateActionId)
+                }
+            }
+        }
     }
 
     override fun setUpBindingVariables() {
@@ -29,7 +35,6 @@ class OnBoardScreenFragment : BaseFragment<FragmentOnBoardScreenBinding>() {
     }
 
     override fun setClickListener() {
-
         binding.login.setOnClickListener {
             if (!binding.etEmail.text.toString().trim().equals(EMAIL, true)) {
                 binding.etEmail.startAnimation(shakeError())
@@ -38,46 +43,10 @@ class OnBoardScreenFragment : BaseFragment<FragmentOnBoardScreenBinding>() {
                 binding.etPass.startAnimation(shakeError())
                 showInfoIconMessage("Please enter valid password")
             } else {
-                lifecycleScope.launchWhenResumed {
+                lifecycleScope.launch {
                     sessionManager.saveSession(true)
                 }
             }
         }
-
-        binding.motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
-            override fun onTransitionStarted(
-                motionLayout: MotionLayout,
-                i: Int,
-                i1: Int
-            ) { /*Not yet implemented*/
-            }
-
-            override fun onTransitionChange(
-                motionLayout: MotionLayout,
-                i: Int,
-                i1: Int,
-                v: Float
-            ) { /*Not yet implemented*/
-            }
-
-            override fun onTransitionCompleted(motionLayout: MotionLayout, i: Int) {
-                lifecycleScope.launch {
-                    repeatOnLifecycle(Lifecycle.State.STARTED) {
-                        sessionManager.isLoggedIn.collect {
-                            if (it)
-                                findNavController().navigate(FragmentNavigator.OPEN_HOME_SCREEN_FROM_ON_BOARD_SCREEN.navigateActionId)
-                        }
-                    }
-                }
-            }
-
-            override fun onTransitionTrigger(
-                motionLayout: MotionLayout,
-                i: Int,
-                b: Boolean,
-                v: Float
-            ) { /*Not yet implemented*/
-            }
-        })
     }
 }
