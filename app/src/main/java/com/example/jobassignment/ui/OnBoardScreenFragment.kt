3cc187ca72aa21c01113f481897614d14020a728
Raj Pratap Singh Jadon <raj.pratap@applaunch.io>
2021-11-30 12:00:28 +0530

    add motion layout animation

--- app/src/main/java/com/example/jobassignment/ui/OnBoardScreenFragment.kt
+++ app/src/main/java/com/example/jobassignment/ui/OnBoardScreenFragment.kt
@@ -1,6 +1,22 @@
 package com.example.jobassignment.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.jobassignment.databinding.FragmentOnBoardScreenBinding
 
 class OnBoardScreenFragment : Fragment() {
+    private lateinit var binding: FragmentOnBoardScreenBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentOnBoardScreenBinding.inflate(inflater)
+        return binding.root
+    }
 }
